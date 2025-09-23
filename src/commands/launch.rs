802088617878524a31b46356e3fcbfb9592a42aa
artrixdotdev <sefu786@outlook.com>
2025-09-22 17:55:47 -0700

    feat: Implement `launch` command

--- src/commands/launch.rs
+++ src/commands/launch.rs
@@ -1,6 +1,33 @@
-use anyhow::Result;
+use crate::{BrowserConfig, Settings};
+use anyhow::{Context, Result, ensure};
+use std::process::Command;
 use url::Url;
 
-pub fn run(url: Url) -> Result<()> {
+pub fn run(settings: Settings, url: Url) -> Result<()> {
+   let url = url.to_string();
+
+   let browsers = settings.browsers;
+
+   let matching_browser = settings
+      .rules
+      .iter()
+      .find(|rule| rule.regex.is_match(&url))
+      .map(|rule| rule.browser.clone())
+      .unwrap_or(settings.default);
+
+   ensure!(
+      browsers.contains_key(&matching_browser),
+      format!("browser {} not found", matching_browser)
+   );
+
+   let browser = browsers.get(&matching_browser).unwrap(); // Safe unwrap because of above code
+
+   let browser = match browser {
+      BrowserConfig::Simple(path) => Command::new(path).arg(&url).spawn(),
+      BrowserConfig::Detailed { path, args } => Command::new(path).args(args).arg(&url).spawn(),
+   };
+
+   browser.with_context(|| format!("failed to launch browser for {}", url))?;
+
    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 mod cli;
 mod commands;
-use anyhow::Result;
+use std::fs;
+
+use anyhow::{Result, ensure};
 use clap::Parser;
 use cli::{Cli, Commands};
 use commands::*;
+use config::{Config, File as ConfigFile};
+
+use settings::*;
 mod settings;
 
 fn main() -> Result<()> {
    let cli = Cli::parse();
+   let config_path = fs::canonicalize(cli.config.unwrap_or("~/browsermux/config.toml".into()))?;
+
+   ensure!(config_path.exists(), "config file not found");
+   let config_path = config_path.to_string_lossy().to_string();
+
+   let config = Config::builder()
+      .add_source(ConfigFile::with_name(&config_path))
+      .build()?;
+
+   let settings = config.try_deserialize::<Settings>()?;
 
    match cli.command {
       Some(Commands::Completions { shell }) => {
          completions::run(shell)?;
       }
-      None => launch::run(cli.url)?,
+      None => launch::run(settings, cli.url)?,
    }
    Ok(())
 }
